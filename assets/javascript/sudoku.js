530d002581da7d7ce69e3eff85f62095f93dffff
Derek Louie <derekl@yahoo-inc.com>
2014-07-16 13:19:11 -0700

    Large code cleanup:
    1) Decoupled the Javascript and HTML a bit more
    2) Cleaned up the CSS a little
    3) Made the Javascript a bit more readable, and efficient
    4) Moved from delegate to on (per the deprication of delegate)
    5) Etc bits of cleanup / optimization

--- assets/javascript/sudoku.js
+++ assets/javascript/sudoku.js
@@ -1,18 +1,18 @@
-// (function() {
+(function() {
     var rows = [],
     columns = [],
     blocks = [],
     lastHighlighted,
     selectedValue,
-    inputTarget,
-    currentTarget,
+    lastInput,
+    currentInput,
     numberSelector = $('.number-selector'),
     numberSelectorArrow = $('.arrow-up'),
     numberSelectorOpen = false,
     cleaningUp = false,
     inputs,
     spread = [1,2,3,4,5,6,7,8,9],
-    aarowFunctions = {
+    arrowFunctions = {
         37: moveLeft,
         38: moveUp,
         39: moveRight,
@@ -29,50 +29,56 @@
         'ZERO': 48,
         'NINE': 57
     };
-    
+
     function initGrid() {
+        //i item bad var names
         var column, block, blockNumber = 0, blockRow = 0;
-        $('.sudoku-row').each( function(i,item) {
-            inputs = $(item).find('input');
+        $('.sudoku-row').each( function(rowNumber,rowInputs) {
+            inputs = $(rowInputs).find('input');
             rows.push(inputs);
-            blockRow = Math.floor(i/3)*3;
-            inputs.each( function(i,item) {
-                column = columns[i];
-                blockNumber = Math.floor(i/3)+blockRow;
+            blockRow = Math.floor(rowNumber/3)*3;
+            inputs.each( function(columnNumber,input) {
+                column = columns[columnNumber];
+                blockNumber = Math.floor(columnNumber/3)+blockRow;
                 block = blocks[blockNumber];
 
                 if (block) {
-                    blocks[blockNumber].push(item);
+                    blocks[blockNumber].push(input);
                 } else {
-                    blocks[blockNumber] = [item];
+                    blocks[blockNumber] = [input];
                 }
 
                 if (column) {
-                    columns[i].push(item);
+                    columns[columnNumber].push(input);
                 } else {
-                    columns[i] = [item];
+                    columns[columnNumber] = [input];
                 }
+
+                input = $(input);
+                input.data('row', rowNumber);
+                input.data('column', columnNumber);
+                input.data('block', blockNumber);
 
             });
         });
         fillRandom();
     }
-    
+
     function bindUI() {
         if (Modernizr.touch) {
             // If mobile disable keyboard from popping up and
             // taking up precious game space
             $('input').prop('disabled', true);
-            // todo: to many random numbers here 
+            // todo: to many random numbers here
             $('.main').on('vclick', function(e) {
-                var currentThird, currentColumn, aarowCss;
-                currentTarget = $(e.target);
-                if( currentTarget.prop('tagName') === 'INPUT' && (!numberSelectorOpen || inputTarget !== e.target) && !cleaningUp ) {
+                var currentThird, currentColumn;
+                currentInput = $(e.target);
+                if( currentInput.prop('tagName') === 'INPUT' && (!numberSelectorOpen || lastInput !== e.target) && !cleaningUp ) {
                         cleanupHighlight();
                         numberSelectorOpen = true;
-                        inputTarget = e.target;
-                        currentColumn = parseColumn(inputTarget);
-                        currentRow = parseRow(inputTarget);
+                        lastInput = e.target;
+                        currentColumn = parseColumn(lastInput);
+                        currentRow = parseRow(lastInput);
                         currentThird = Math.floor(currentColumn/3);
                         leftOffset = (-53.6 + (currentThird*54.8));
                         // We want to keep the page from expanding past the viewport
@@ -84,35 +90,35 @@
                             numberSelectorArrow.removeClass('flip-arrow-down');
                             topOffset = (((currentRow+1) * 9)+7);
                         }
-                        lastHighlighted = $(inputTarget).addClass("highlighted");
+                        lastHighlighted = $(lastInput).addClass("highlighted");
                         numberSelectorArrow.css({left: (3.225+((currentColumn%3)*9))+'rem'});
-                        numberSelector.css({top: topOffset+'rem', left: leftOffset+'rem'});
+                        numberSelector.css({top: topOffset+'rem', left: leftOffset+'rem', display: 'inline'});
                     } else {
                         cleanupNumberSelector();
                     }
             });
-    
+
             numberSelector.on('vclick', function(e) {
-                selectedValue = $(e.target).data('value');
-                $(inputTarget).val(selectedValue);
+                selectedValue = parseInt($(e.target).html());
+                $(lastInput).val(selectedValue);
                 cleanupNumberSelector();
                 // Make sure if number selector is over the buttons at the bottom
                 // the buttons aren't activated
                 e.preventDefault();
             });
         }
-    
+
         // Mask for inputs to stop users from putting in invalid
-        // values and also handlers for aarow navigation
-    
-        $('.sudoku-row').delegate('input', 'keydown', function(ev) {
-            var keyCode = window.event ? ev.keyCode : ev.which,
-            move = aarowFunctions[keyCode];
-    
+        // values and also handlers for arrow navigation
+
+        $('.sudoku-row').on('keydown', 'input', function(e) {
+            var keyCode = e.which;
+            move = arrowFunctions[keyCode];
+
             if(move) {
-                move(ev);
+                move(e);
             }
-            
+
             // Only let through 0-9 through
             if (keyCode < keyCodes.ZERO || keyCode > keyCodes.NINE) {
 
@@ -129,28 +135,28 @@
                         break;
                     // Otherwise we make sure it doesn't get through
                     default:
-                        ev.preventDefault();
+                        e.preventDefault();
                 }
             }
         });
-    
-        $('.clear').on('click', promptClear);
-    
+
+        $('.clear').on('click', confirmClear);
+
         $('.check-answer').on('click', checkAnswer);
-    
+
     }
-    
+
     function cleanupHighlight() {
         if (lastHighlighted) {
             $('.highlighted').removeClass('highlighted');
         }
     }
-    
+
     function cleanupNumberSelector() {
         selectedValue = '';
-        numberSelector.css({left:'-10000px'});
+        numberSelector.css({display:'none'});
         cleanupHighlight();
-    
+
         if (!cleaningUp) {
             cleaningUp = true;
             // We debounce touch events so users tapping very quickly
@@ -158,72 +164,72 @@
             setTimeout(function() {numberSelectorOpen = false; cleaningUp = false;}, 500);
         }
     }
-    
+
     function parseRow(node) {
-        return Math.floor($(node.parentNode.parentNode.parentNode).data('row'));
+        return $(node).data('row');
     }
-    
+
     function parseColumn(node) {
-        return Math.floor($(node).data('column'));
+        return $(node).data('column');
     }
-    
-    function moveLeft(ev) {
-        var row = parseRow(ev.target),
-            column = parseColumn(ev.target),
+
+    function moveLeft(e) {
+        var row = parseRow(e.target),
+            column = parseColumn(e.target),
             leftNeighbor = columns[column-1];
-    
+
         if (leftNeighbor) {
             $(columns[column-1][row]).focus();
         }
     }
-    
-    function moveRight(ev) {
-        var row = parseRow(ev.target),
-            column = parseColumn(ev.target),
+
+    function moveRight(e) {
+        var row = parseRow(e.target),
+            column = parseColumn(e.target),
             rightNeighbor = columns[column+1];
-    
+
         if (rightNeighbor) {
             $(columns[column+1][row]).focus();
         }
     }
-    
-    function moveUp(ev) {
-        var row = parseRow(ev.target),
-            column = parseColumn(ev.target),
+
+    function moveUp(e) {
+        var row = parseRow(e.target),
+            column = parseColumn(e.target),
             topNeighbor = rows[row-1];
-    
+
         if (topNeighbor) {
             $(columns[column][row-1]).focus();
         }
     }
-    
-    function moveDown(ev) {
-        var row = parseRow(ev.target),
-            column = parseColumn(ev.target),
+
+    function moveDown(e) {
+        var row = parseRow(e.target),
+            column = parseColumn(e.target),
             bottomNeighbor = rows[row+1];
-    
+
         if (bottomNeighbor) {
             $(columns[column][row+1]).focus();
         }
     }
-    
+
     function checkHasRangeOneToTen(l) {
-        var inputValues, 
+        var inputValues,
             val,
-            answer = false;
-    
+            answer = false,
+            len;
+
         $.each(l, function(i, item) {
-    
+
             inputValues = $.map(item, function(input) {
                 return input.value;
             });
-    
+
             inputValues.sort();
-            len = inputValues.length; 
-            val = 9;
-    
+            len = inputValues.length;
+
             while(len--) {
-                if (Math.floor(inputValues[len]) !== val--) {
+                if (parseInt(inputValues[len]) !== len+1) {
                     $(item).addClass('error');
                     answer = false;
                     // Break out early
@@ -233,63 +239,58 @@
                 }
             }
         });
-    
+
         return answer;
     }
-    
+
     function checkAnswer() {
         if (checkHasRangeOneToTen(rows) && checkHasRangeOneToTen(columns) && checkHasRangeOneToTen(blocks)) {
             if (window.confirm('Great job you won! Would you like to play again?')) {
-                fillRandom(); 
+                fillRandom();
             }
         } else {
-            promptContinue(); 
+            alertContinue();
         }
-    } 
-    
-    function promptContinue() {
+    }
+
+    function alertContinue() {
         window.alert("It looks like you aren't quite done yet!");
         $('.error').removeClass('error');
     }
-    
-    function promptClear() {
+
+    function confirmClear() {
         if (window.confirm('Do you really want to clear your game?')) {
             fillRandom();
         }
     }
-    
+
     function clearGrid() {
         // clean up error classes
         $('.error').removeClass('error');
-    
-        $.each(rows, function(i,item) {
-            $.each(item, function(i,cell) {
-                cell.value = '';
-            });
-        });
+        $('input').val('');
     }
-    
+
     function fillRandom() {
         clearGrid();
-    
+
         var temp = [],
             rand,
             shuffled;
-    
+
         // Insert the numbers 1-9 into the grid
         $.each(rows, function(i,row) {
             rand = Math.floor(Math.random()*9);
             row[rand].value = spread[i];
             temp.push(rand);
         });
-    
+
         // Shuffle our numbers so the next grid generated
         // is more random
         $.each(temp, function(i,item) {
             spread.push(spread.splice(item,1)[0]);
         });
     }
-    
+
     initGrid();
     bindUI();
-// })();
+})();
